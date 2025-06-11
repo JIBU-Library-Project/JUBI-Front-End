d2b102aff32a07d3956d9d54fa6ea846da06012b
Umar Mohammed <umarmohammed2227@gmail.com>
2025-06-11 16:30:19 +0200

    updated

--- src/components/AdminNavBar.jsx
+++ src/components/AdminNavBar.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from "react";
+import { Link } from "react-router";
 
 const AdminNavBar = () => {
   return (
     <nav className=" mt-4 p-2 //bg-amber-500 flex flex-row justify-between items-center pl-1">
-       <span className="ml-10 font-extrabold text-[25px] ">JUBILib.</span>
+      <span className="ml-10 font-extrabold text-[25px] ">JUBILib.</span>
       <ul className=" pl-15 pr-15 flex gap-x-15 bg-[#1a1c19] text-black rounded-[2rem] border-amber-200 py-[10px] px-[1.5em]">
-
-         <Link to={"/admin-booklist"}>
-          <li className=" text-white px-[1.5em] font-medium hover:bg-[#cdf44b] hover:text-[#1a1c19] //bg-amber-300 p-2 rounded-full">All Books</li>
+        <Link to={"/admin-booklist"}>
+          <li className=" text-white px-[1.5em] font-medium hover:bg-[#cdf44b] hover:text-[#1a1c19] //bg-amber-300 p-2 rounded-full">
+            All Books
+          </li>
         </Link>
         <Link to={"/add-book"}>
-          <li className=" text-white px-[1.5em] font-medium hover:bg-[#cdf44b] hover:text-[#1a1c19] //bg-amber-300 p-2 rounded-full">Add Book</li>
+          <li className=" text-white px-[1.5em] font-medium hover:bg-[#cdf44b] hover:text-[#1a1c19] //bg-amber-300 p-2 rounded-full">
+            Add Book
+          </li>
         </Link>
         <Link to={"/edit-book"}>
-          <li className=" text-white px-[1.5em] font-medium hover:bg-[#cdf44b] hover:text-[#1a1c19] //bg-amber-300 p-2 rounded-full">Edit Book</li>
+          <li className=" text-white px-[1.5em] font-medium hover:bg-[#cdf44b] hover:text-[#1a1c19] //bg-amber-300 p-2 rounded-full">
+            Edit Book
+          </li>
         </Link>
-        
       </ul>
       <Link to="/">
         <button className="bg-[#cbfb45] font-medium text-black rounded-[2rem] border-amber-200 py-[10px] px-[1.5em] mr-10 hover:bg-[#cdf44b] hover:text-[#1a1c19] ">
@@ -24,7 +28,7 @@ const AdminNavBar = () => {
         </button>
       </Link>
     </nav>
-  )
-}
+  );
+};
 
-export default AdminNavBar
+export default AdminNavBar;
